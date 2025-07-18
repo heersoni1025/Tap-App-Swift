97989e34afdf4135db8dd70c818df84947d66f46
Heer Soni <100152930+heersoni1025@users.noreply.github.com>
2025-07-17 23:48:43 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TapGame
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var possiblePics = ["apple", "dog", "egg"]
+    
+    var body: some View {
+        VStack {
+            Image(possiblePics[0]).resizable().aspectRatio(contentMode: .fit).frame(height : 300)
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
