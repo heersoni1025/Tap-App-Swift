fef6172261ac1bcd806137414221da7f5f80c2e0
Heer Soni <100152930+heersoni1025@users.noreply.github.com>
2025-08-05 23:26:45 -0400

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,22 +1,37 @@
-
+//
 //  ContentView.swift
-//  TapGame in progress
-//come back for more updates
+//  TapGame
+// in progress, come back for updates
+//
 
 import SwiftUI
 
 struct ContentView: View {
     
-    @State var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var currentPicIndex = 0
     
     var possiblePics = ["apple", "dog", "egg"]
     
     var body: some View {
         VStack {
-            Image(possiblePics[0]).resizable().aspectRatio(contentMode: .fit).frame(height : 300)
+            Image(possiblePics[currentPicIndex]).resizable().aspectRatio(contentMode: .fit).frame(height : 300)
         }
-        
+        .onReceive(timer, perform: { _ in
+            changePic()
+            
+        })
     }
+    
+    func changePic() {
+        if currentPicIndex == possiblePics.count - 1{
+            currentPicIndex = 0
+        }
+        else{
+            currentPicIndex += 1
+        }}
+    
 }
 
 #Preview {
